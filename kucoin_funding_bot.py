5b1feaa604674b3e94423d426c46e3f3b26fdd3c
jumabe80 <jumabe@gmail.com>
2025-05-14 22:38:03 +0200

    Update kucoin_funding_bot.py

--- kucoin_funding_bot.py
+++ kucoin_funding_bot.py
@@ -44,7 +44,8 @@ def get_kucoin_funding_rates():
             volume_usdt = volume_base * mark_price
 
             # Calculate funding countdown in minutes (fixed correctly)
-            time_to_funding_min = int((next_funding_ts - now_ms) / 60000)
+            now_sec = int(time.time())
+            time_to_funding_min = int((next_funding_ts - now_sec) / 60)
 
             if funding_rate >= FUNDING_RATE_THRESHOLD and volume_usdt >= VOLUME_24H_THRESHOLD:
                 results.append({
