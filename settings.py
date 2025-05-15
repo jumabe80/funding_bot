dad310a49772fadba3e9400e1f4ea0137401bb6b
jumabe80 <jumabe@gmail.com>
2025-05-15 23:09:08 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # Umbrales configurables para filtrar oportunidades
 
 FUNDING_RATE_THRESHOLD = 0.0005  # 0.05%
-VOLUME_24H_THRESHOLD = 2000000  # 2 millones
+VOLUME_24H_THRESHOLD = 1000000  # 1 million
